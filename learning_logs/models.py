bc54e75fe2a33833446ffc0a60bb4cd609f8389f
Raul <raulmayas20@gmail.com>
2022-02-01 15:20:17 -0600

    Developing Entry Model and ReadMe Update

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -11,3 +11,17 @@ class Topic(models.Model):
     def __str__(self):
         '''Returns a string in representation of a model'''
         return self.text
+
+class Entry(models.Model):
+    '''New information/knowledge about the topic'''
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verpose_name_plural = 'entries'
+
+    def __str__(self):
+        '''Returns a string in representation of a model'''
+        return f"{self.text[:50]}..."
+        
