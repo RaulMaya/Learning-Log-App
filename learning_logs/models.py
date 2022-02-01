0cc99b63231542a0a5dc9a1959f53d64e68e0fd6
Raul <raulmayas20@gmail.com>
2022-02-01 13:32:26 -0600

    Creating Class For My Topic

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Topic(models.Model):
+    '''This corresponds to the topic that the user is learning about'''
+    text = models.CharField(max_length=250)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        '''Returns a string in representation of a model'''
+        return self.text
