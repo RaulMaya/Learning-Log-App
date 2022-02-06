790e951c50dc542d5bcf35f680f29517564eb51f
Raul <raulmayas20@gmail.com>
2022-02-06 11:28:28 -0600

    New Topic Function Done

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -8,4 +8,13 @@ app_name = 'learning_logs'
 urlpatterns = [
     # Home Page
     path('', views.index, name='index'),
+
+    # Page that shows all the topics
+    path('topics', views.topics, name='topics'),
+
+    # Detail page for a single topic
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
+
+    # Page for adding a new topic
+    path('new_topic/', views.new_topic, name='new_topic'),
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,7 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+import learning_logs
+
+from .models import Topic
+from .forms import TopicForm
 
 # Create your views here.
 
 def index(request):
     """The home page for learning logs"""
     return render(request, 'learning_logs/index.html')
+
+def topics(request):
+    """Show all the topics"""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
+
+def topic(request, topic_id):
+    """Shows the topic, with all his entries"""
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context ={'topic':topic, 'entries':entries}
+    return render(request, 'learning_logs/topic.html', context)
+
+def new_topic(request):
+    """Adding a new topic"""
+    if request.method != 'POST':
+        # No data submitted; and create a blank form
+        form = TopicForm()
+    else:
+        # POST data submitted; Processing data
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topics')
+    # Display a blanck or invalid form
+    context = {'form':form}
+    return render(request, 'learning_logs/new_topic.html', context)
