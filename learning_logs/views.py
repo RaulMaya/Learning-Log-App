426d88288726e9506a14e7c1b22e0b6e10721ed5
Raul <raulmayas20@gmail.com>
2022-02-11 12:36:47 -0600

    Creating the new entries html

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,9 +1,10 @@
+from email import contentmanager
 from django.shortcuts import render, redirect
 
 import learning_logs
 
 from .models import Topic
-from .forms import TopicForm
+from .forms import TopicForm, EntryForm
 
 # Create your views here.
 
@@ -32,9 +33,30 @@ def new_topic(request):
     else:
         # POST data submitted; Processing data
         form = TopicForm(data=request.POST)
+
         if form.is_valid():
             form.save()
             return redirect('learning_logs:topics')
     # Display a blanck or invalid form
     context = {'form':form}
     return render(request, 'learning_logs/new_topic.html', context)
+
+def new_entry(request, topic_id):
+    """Adding a new entry for an specific topic"""
+    topic = Topic.objects.get(id=topic_id)
+
+    if request.method != 'POST':
+        # No data submitted; Create a blank form
+        form = EntryForm()
+    else:
+        # POST data submitted; Processing data
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return redirect('learning_logs:topic', topic_id = topic_id)
+
+    # Display a blank or invalid form.
+    context = {'topic': topic, 'form': form}
+    return render(request, 'learning_logs/new_entry.html', context)
