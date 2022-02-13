3ac84e2609e12d1008489e022c63175beacbcb97
Raul <raulmayas20@gmail.com>
2022-02-12 21:28:49 -0600

    Registration page uploaded

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,12 @@
 """Defines URL patterns for the users"""
 
 from django.urls import path, include
+from . import views
 
 app_name = 'users'
 urlpatterns = [
     # Include the default authentication urls
     path('', include('django.contrib.auth.urls')),
+    # Registration of new users
+    path('register/', views.register, name="register"),
 ]
